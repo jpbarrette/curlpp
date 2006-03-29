c44811bf956d76e666cab4dbe6f3318337d9ad30
jpbarrette <jpbarrette@localhost>
2006-03-29 15:51:13 +0000

    [curlpp-svn @ 15] SList bug fixed

---
+++ examples/example12.cpp
@@ -0,0 +1,68 @@
+/*
+ *    Copyright (c) <2002-2005> <Jean-Philippe Barrette-LaPierre>
+ *    
+ *    Permission is hereby granted, free of charge, to any person obtaining
+ *    a copy of this software and associated documentation files 
+ *    (cURLpp), to deal in the Software without restriction, 
+ *    including without limitation the rights to use, copy, modify, merge,
+ *    publish, distribute, sublicense, and/or sell copies of the Software,
+ *    and to permit persons to whom the Software is furnished to do so, 
+ *    subject to the following conditions:
+ *    
+ *    The above copyright notice and this permission notice shall be included
+ *    in all copies or substantial portions of the Software.
+ *    
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ *    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+ *    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ *    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <errno.h>
+
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
+
+int main(int argc, char *argv[])
+{
+  if(argc < 2) {
+    std::cerr << "Example 11: Wrong number of arguments" << std::endl 
+	      << "Example 11: Usage: example12 url" 
+	      << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  char *url = argv[1];
+  
+  try {
+    cURLpp::Cleanup cleaner;
+    cURLpp::Easy request;
+    
+    request.setOpt(new cURLpp::Options::Url(url)); 
+    request.setOpt(new cURLpp::Options::Verbose(true)); 
+    
+    std::list<std::string> header; 
+    header.push_back("Content-Type: application/octet-stream"); 
+    
+    request.setOpt(new cURLpp::Options::HttpHeader(header)); 
+    
+    request.setOpt(new cURLpp::Options::PostFields("abcd"));
+    request.setOpt(new cURLpp::Options::PostFieldSize(5));
+    
+    request.perform(); 
+  }
+  catch ( cURLpp::LogicError & e ) {
+    std::cout << e.what() << std::endl;
+  }
+  catch ( cURLpp::RuntimeError & e ) {
+    std::cout << e.what() << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
